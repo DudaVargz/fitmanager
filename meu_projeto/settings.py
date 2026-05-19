783fee6fbcf603fb8917457a2ae2372e0d23c707
Duda Vargas <eduardav.vargas14@gmail.com>
2026-05-19 13:50:58 -0300

    fix: migra para PostgreSQL no deploy

--- meu_projeto/settings.py
+++ meu_projeto/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 from decouple import config
+import dj_database_url
+import os
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -52,14 +55,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'meu_projeto.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
