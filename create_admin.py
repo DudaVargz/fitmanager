345023ea8e70756826ac6aa75b08a76d91d1e065
Duda Vargas <eduardav.vargas14@gmail.com>
2026-05-19 14:50:41 -0300

    fix: recria superusuario no deploy

--- create_admin.py
+++ create_admin.py
@@ -7,8 +7,6 @@ django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@fitmanager.com', 'fitmanager123')
-    print('Superusuário criado!')
-else:
-    print('Já existe.')
+User.objects.filter(username='admin').delete()
+User.objects.create_superuser('admin', 'admin@fitmanager.com', 'fitmanager123')
+print('Superusuário criado!')
